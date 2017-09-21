a58e601b93903d46971224c19053ee22769cca78
ccpattyliu <cc.pattyliu@gmail.com>
2017-09-21 10:34:26 +0800

    改用PHP來做網頁囉~

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>Holle
+    <meta charset="UTF-8">
+    <title>Title~~~~php</title>
+</head>
+<body>
+<h1>哈囉~~~~~</h1>
+<?php
+    echo "this is ......"
+?>
+</body>
+</html>
