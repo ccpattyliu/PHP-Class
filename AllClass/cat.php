debfc185e88ded676c847f9e2e580de12c0b7f08
ccpattyliu <cc.pattyliu@gmail.com>
2017-10-26 16:38:59 +0800

    繼承及呼叫父類別的建構子

--- AllClass/cat.php
+++ AllClass/cat.php
@@ -9,13 +9,20 @@ class cat extends Animal
     {
         parent::__construct("貓");//需要呼叫父類別初始化的方法，否則父類別不會做預定的初始值動作
         $this->petName=$petNameFromOut;
+        $this->position;
     }
     public function getPetName(){
        //$this->getAnimalType();
         return $this->petName;
     }
 
+
+
 }
 
 $cat=new cat("kitty");
 echo $cat->getAnimalType();//繼承後可以直接使用父類別的public 方法
+
+$cat->getFood("小魚");
+$cat->moveDxDy(1,1);
+$cat->moveDxDy(1,2);
